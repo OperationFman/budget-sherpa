74c8704eddf398e4c4da6a32d8956bf1b3f80880
OperationFman <franklin.m.moon@gmail.com>
2023-09-29 22:16:23 +1000

    :sparkles: Add <Header /> to the UI

---
+++ ui/app/components/Header.tsx
@@ -0,0 +1,30 @@
+import styles from "./Header.module.scss";
+import EditIcon from "@mui/icons-material/Edit";
+
+export const Header = () => {
+	return (
+		<div className={styles.header}>
+			<div className={styles.pageContainer}>
+				<h3 className={styles.banner}>Budget 🎒 Sherpa</h3>
+
+				<div className={styles.overviewContainer}>
+					<div className={styles.overviewNumbersContainer}>
+						<div>
+							<span className={styles.overviewHeading}>$ 8,300</span>
+							<span className={styles.overviewSlash}>/</span>
+							<span className={styles.overviewSubheading}>$ 15,000</span>
+						</div>
+
+						<div className={styles.headerMobileEditButton}>
+							<EditIcon fontSize='medium' />
+						</div>
+					</div>
+					<div className={styles.budgetRemainingText}>$6,700 Under-Budget</div>
+					<div className={styles.headerDesktopEditButton}>
+						<EditIcon fontSize='medium' />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
