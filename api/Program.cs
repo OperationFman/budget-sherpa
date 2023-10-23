7653f22f8dea42aa6e28b7da37e177c374fb861f
OperationFman <franklin.m.moon@gmail.com>
2023-10-23 19:12:22 +0700

    :alembic: Remove slash from origin domain

--- api/Program.cs
+++ api/Program.cs
@@ -9,16 +9,17 @@ builder.Services.AddCors(options =>
         builder =>
         {
             builder
-            .WithOrigins("https://budget-sherpa-ui.onrender.com/")
+            .WithOrigins("https://budget-sherpa-ui.onrender.com")
             .AllowAnyMethod()
             .AllowAnyHeader();
+
         });
 
 });
 
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -28,7 +29,6 @@ builder.Services.AddDbContext<ApiDbContext>(
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
